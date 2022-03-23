78b9f4751a26e4e31b7280f9922cb239ef4e07e8
KwonSunwon <kso00011147@gmail.com>
2022-03-24 03:17:55 +0900

    파일입출력 오류
    예상원인
    1. fwrite 함수로 파일에 저장 할 때 제대로 된 값이 저장 안되고 잘못된 값이 저장됨
    2. 저장은 제대로 되었지만 fread 함수에서 읽어오는 방식이 잘못됨

--- Proj_04_KSW/Proj_04_KSW/main.c
+++ Proj_04_KSW/Proj_04_KSW/main.c
@@ -4,10 +4,13 @@
 // 문제 2번. MAX_NUMBER 로 정의해서 크기 결정
 // fread, fwrite
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define PROB 1
+#define PROB 2
 
 #if PROB == 1
 
@@ -164,5 +167,183 @@ node *mulPoly(node *a, node *b)
 }
 
 #elif PROB == 2
+
+#define FNAME "namecard.bin"
+#define MAX_NUMBER 10
+
+typedef struct nameCard
+{
+    char name[5];
+    int company;
+    int phone;
+} card;
+
+void printf_menu();
+
+int init(card *);
+void finalize(card *, int);
+
+void addCard(card *, int);
+void printCard(card *, int);
+void searchCard(card *, int);
+void delCard(card *, int *);
+
+int main(void)
+{
+    card NC[MAX_NUMBER] = {'\0', 0, 0};
+
+    // char answer;
+    int menuItem;
+    int count = 0;
+    count = init(NC);
+
+    while (1)
+    {
+        printf_menu();
+        scanf("%d", &menuItem);
+        printf("\n");
+        switch (menuItem)
+        {
+        case 1:
+            printf("명함 입력\n");
+            printf("---------------------\n");
+            addCard(NC, count++);
+            break;
+        case 2:
+            printf("명함 출력\n");
+            printf("---------------------\n");
+            printCard(NC, count);
+            break;
+        case 3:
+            printf("명함 검색\n");
+            printf("---------------------\n");
+            searchCard(NC, count);
+            break;
+        case 4:
+            printf("명함 삭제\n");
+            printf("---------------------\n");
+            delCard(NC, &count);
+            break;
+        case 5:
+            printf("프로그램 종료\n");
+            finalize(NC, count);
+            return;
+        }
+    }
+}
+
+void printf_menu()
+{
+    printf("\n=============Name Card DBMS=============\n");
+    printf("명령어를 선택하세요.\n");
+    printf("1. 명함 입력\n");
+    printf("2. 모든 명함 출력\n");
+    printf("3. 명함 검색\n");
+    printf("4. 명함 삭제\n");
+    printf("5. 종료\n");
+    printf(" 명령 입력 ---> ");
+}
+
+int init(card *NC)
+{
+    FILE *fp = fopen(FNAME, "rb");
+    if (fp == NULL)
+    {
+        printf("새로운 파일 생성\n");
+        fp = fopen(FNAME, "w+");
+        fclose(fp);
+        return 0;
+    }
+    int count = 0;
+    fseek(fp, 0, SEEK_END);
+    count = ftell(fp) / sizeof(card);
+    fread(NC, sizeof(card), count, fp);
+    fclose(fp);
+    return count;
+}
+
+void finalize(card *NC, int count)
+{
+    if (count == 0)
+        return;
+
+    FILE *fp = fopen(FNAME, "wb");
+    if (fp == NULL)
+    {
+        printf("파일 오픈 실패\n");
+        exit(-1);
+    }
+    fwrite(NC, sizeof(card), count, fp);
+    fclose(fp);
+}
+
+void addCard(card *NC, int count)
+{
+    char name[4] = {'\0'};
+    int company, phone;
+    printf("이름 : ");
+    scanf("%s", &name);
+    printf("회사 : ");
+    scanf("%d", &company);
+    printf("전화 : ");
+    scanf("%d", &phone);
+
+    strcpy(NC[count].name, name);
+    NC[count].company = company;
+    NC[count].phone = phone;
+}
+
+void printCard(card *NC, int count)
+{
+    printf("모든 명함 출력\n");
+    for (int i = 0; i < count; ++i)
+        printf("%d. 이름:%s, 회사:%d, 전화:%d\n", i, NC[i].name, NC[i].company, NC[i].phone);
+    printf("%d개의 명함이 존재함\n", count);
+}
+
+void searchCard(card *NC, int count)
+{
+    char key[4];
+    printf("검색할 이름을 입력하세요. : ");
+    scanf("%s", key);
+
+    int searchCount = 0;
+
+    for (int i = 0; i < count; ++i)
+    {
+        if (strstr(NC[i].name, key) != NULL)
+        {
+            printf("검색한 명함 이름 : ");
+            printf("이름:%s, 회사:%d, 전화:%d\n", NC[i].name, NC[i].company, NC[i].phone);
+            ++searchCount;
+        }
+    }
+    printf("%d개의 명함 검색함\n", searchCount);
+}
+
+void delCard(card *NC, int *count)
+{
+    char key[4];
+    printf("삭제할 이름을 입력하세요. : ");
+    scanf("%s", key);
+
+    int delCount = 0;
+
+    for (int i = 0; i < *count; ++i)
+    {
+        if (strcmp(NC[i].name, key) == 0)
+        {
+            printf("삭제 명함 이름 : ");
+            printf("이름:%s, 회사:%d, 전화:%d\n", NC[i].name, NC[i].company, NC[i].phone);
+            for (int j = i; j < *count - 1; ++j)
+            {
+                NC[j] = NC[j + 1];
+            }
+            --*count;
+            ++delCount;
+        }
+    }
+    printf("%d개의 명함 삭제함", delCount);
+}
 
 #endif
