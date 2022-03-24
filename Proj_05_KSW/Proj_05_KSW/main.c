1cfa1295194ace2b72da1f65738fa4d4a348d2b0
KwonSunwon <kso00011147@gmail.com>
2022-03-24 22:03:48 +0900

    5번 문제 해결

--- Proj_05_KSW/Proj_05_KSW/main.c
+++ Proj_05_KSW/Proj_05_KSW/main.c
@@ -22,15 +22,13 @@ void push(stack *, element);
 element pop(stack *);
 int isEmpty(stack);
 
-int checkBracket(stack *, element *, int, char);
+int checkBracket(stack *, element *, char);
 
 int main()
 {
     stack stk;
-    char *expr = "A(x) = 24.5 * (x + func(x, y)) * (r * r * 3.14) / [7 % 10 * sqrt(25)];";
+    char *expr = "A(x) = 24.5 * (x + func(x, y)) * (r * r * 3.14) / [ 7 % 10 * sqrt(25)];";
     element temp;
-    int errorPos[MAX_STACK_NUMBER];
-    int errorCount = 0;
 
     init(&stk);
 
@@ -43,24 +41,26 @@ int main()
         case '[':
         case '{':
             temp.bracket = expr[i];
-            temp.pos = i;
+            temp.pos = i + 1;
             push(&stk, temp);
             break;
 
         case ')':
-            if (checkBracket(&stk, &temp, i, expr[i]) == 0)
-                break;
-            else
-            {
-                // 오류난 위치 전달하는거 만들면됨.
-            }
         case ']':
-            checkBracket(&stk, &temp, i, expr[i]);
-            break;
         case '}':
-            checkBracket(&stk, &temp, i, expr[i]);
-            break;
-
+            switch (checkBracket(&stk, &temp, expr[i]))
+            {
+            case 0:
+                break;
+            case 1:
+                printf("%s\n", expr);
+                printf(" => 괄호검사실패 %d 위치\n", i + 1);
+                return;
+            case 2:
+                printf("%s\n", expr);
+                printf(" => 괄호검사실패 %d, %d 위치\n", temp.pos, i + 1);
+                return;
+            }
         default:
             break;
         }
@@ -68,17 +68,15 @@ int main()
         ++i;
     }
     printf("%s\n", expr);
-    if (isEmpty(stk) != 1 || errorCount > 0) // 괄호의 개수가 맞지 않는 경우, 이미 오류가 존재하는 경우
+    if (isEmpty(stk) != 1) // 괄호의 개수가 맞지 않는 경우, 이미 오류가 존재하는 경우
     {
         printf(" => 괄호검사실패 ");
-        for (int i = 0; i < stk.top; ++i)
+        for (int i = 0; i < stk.top + 1; ++i)
             printf("%d ", pop(&stk).pos);
-        for (int i = 0; i < errorCount; ++i)
-            printf("%d ", errorPos[i]);
         printf("위치\n");
     }
     else
-        printf(" => 괄호검사성공");
+        printf(" => 괄호검사성공\n");
 }
 
 void init(stack *s)
@@ -104,15 +102,27 @@ int isEmpty(stack s)
         return 0;
 }
 
-int checkBracket(stack *s, element *e, int i, char bracket)
+int checkBracket(stack *s, element *e, char bracket)
 {
     if (isEmpty(*s) == 1) // 우 괄호가 먼저 나오는 경우
         return 1;
 
     *e = pop(s);
+    switch (bracket)
+    {
+    case ')':
+        bracket = '(';
+        break;
+    case ']':
+        bracket = '[';
+        break;
+    case '}':
+        bracket = '{';
+        break;
+    }
     if (e->bracket != bracket) // 괄호의 종류가 다른 경우
     {
-        return 1;
+        return 2;
     }
     else
         return 0;
