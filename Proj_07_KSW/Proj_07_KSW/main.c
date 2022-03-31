0fa54ceefdec0c362e1719f543dde4b9b8a14c54
KwonSunwon <kso00011147@gmail.com>
2022-04-01 00:24:15 +0900

    07번 해결

--- Proj_07_KSW/Proj_07_KSW/main.c
+++ Proj_07_KSW/Proj_07_KSW/main.c
@@ -3,25 +3,26 @@
 #include <time.h>
 
 #define MAX_QUEUE_SIZE 10
-#define WORK_TIME 10'000
+#define WORK_TIME 10000
 
 // front % MAX_QUEUE_SIZE == (rear + 1) % MAX_QUEUE_SIZE
 
 typedef struct _ELEMENT
 {
-    char menu;
+    int menu;
     int orderTime;
 } ELEMENT;
 
 typedef struct _QUEUE
 {
-    ELEMENT data[MAX_QUEUE_SIZE + 1];
+    ELEMENT data[MAX_QUEUE_SIZE];
     int front;
     int rear;
 } QUEUE;
 
 typedef struct _SERVER
 {
+    ELEMENT order;
     int processingTime;
     int restTime;
 } SERVER;
@@ -34,17 +35,19 @@ int isEmpty(QUEUE);
 void enqueue(QUEUE *, ELEMENT);
 ELEMENT dequeue(QUEUE *);
 
+int menuRequired(int);
+
 int main()
 {
     QUEUE orderQueue;
     initQueue(&orderQueue);
 
-    SERVER server1 = {-1, 0};
-    SERVER server2 = {-1, 0};
+    SERVER server1 = {{-1, 0}, -1, 0};
+    SERVER server2 = {{-1, 0}, -1, 0};
 
     int totalService[5] = {0};
+    int cumulativeTime[5] = {0};
     int cancledOrder = 0;
-    int cumulativeTime = 0;
     float averageTime = 0;
 
     int workTime = 0;
@@ -52,33 +55,67 @@ int main()
 
     while (workTime <= WORK_TIME)
     {
-        if (workTime % 5 == 0)
+        if (workTime % 7 == 0)
         {
-            ELEMENT newOrder = {rand() % 5, workTime};
             if (!isFull(orderQueue))
+            {
+                ELEMENT newOrder = {rand() % 5, workTime};
                 enqueue(&orderQueue, newOrder);
+            }
             else
                 ++cancledOrder;
         }
 
-        if (!server1.processingTime)
+        if (server1.order.menu == -1)
         {
-            ELEMENT newProcess = dequeue(&orderQueue);
-            server1.processingTime = // 각 메뉴의 시간만큼
-            // 여기까지
+            if (!isEmpty(orderQueue))
+            {
+                server1.order = dequeue(&orderQueue);
+                server1.processingTime = menuRequired(server1.order.menu);
+            }
+            else
+                ++server1.restTime;
+        }
+        else if (server1.processingTime == 0)
+        {
+            ++totalService[server1.order.menu];
+            cumulativeTime[server1.order.menu] += workTime - server1.order.orderTime;
+            server1.order.menu = -1;
+        }
+
+        if (server2.order.menu == -1)
+        {
+            if (!isEmpty(orderQueue))
+            {
+                server2.order = dequeue(&orderQueue);
+                server2.processingTime = menuRequired(server2.order.menu);
+            }
+            else
+                ++server2.restTime;
+        }
+        else if (server2.processingTime == 0)
+        {
+            ++totalService[server2.order.menu];
+            cumulativeTime[server2.order.menu] += workTime - server2.order.orderTime;
+            server2.order.menu = -1;
         }
 
         ++workTime;
-        --server1.processingTime;
-        --server2.processingTime;
+        if (server1.order.menu != -1)
+            --server1.processingTime;
+        if (server2.order.menu != -1)
+            --server2.processingTime;
     }
 
     printf("=========== Results ============\n");
+    printf("큐의 갯수 : %d\n", MAX_QUEUE_SIZE);
     for (int i = 0; i < 5; i++)
     {
-        printf("MENU %d %d개 서비스, 누적대기시간 %d, 평균서비스시간 %f\n", i, totalService[i], cumulativeTime, averageTime);
+        averageTime = (float)cumulativeTime[i] / totalService[i];
+        printf("MENU %d %d개 서비스, 누적대기시간 %d, 평균서비스시간 %f\n", i, totalService[i], cumulativeTime[i], averageTime);
     }
     printf("취소된 주문 %d 개\n", cancledOrder);
+    printf("서버1과 서버2 휴식 시간 : %d, %d\n", server1.restTime, server2.restTime);
     printf("================================\n");
 }
 
@@ -106,28 +143,42 @@ int isEmpty(QUEUE q)
 
 void enqueue(QUEUE *q, ELEMENT e)
 {
-    if (isFull)
+    if (isFull(*q))
         printf("Queue is full\n");
     else
     {
-        if (q->rear == MAX_QUEUE_SIZE + 1)
-            q->rear = 0;
-        else
-            ++q->rear;
+        q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
         q->data[q->rear] = e;
     }
 }
 
 ELEMENT dequeue(QUEUE *q)
 {
-    if (ifEmpty(q))
+    if (isEmpty(*q))
         printf("Queue is empty\n");
     else
     {
-        if (q->front == MAX_QUEUE_SIZE + 1)
-            q->front = 0;
-        else
-            ++q->front;
+        q->front = (q->front + 1) % MAX_QUEUE_SIZE;
         return q->data[q->front];
+    }
+}
+
+int menuRequired(int menuNum)
+{
+    switch (menuNum)
+    {
+    case 0:
+        return 5;
+    case 1:
+        return 10;
+    case 2:
+        return 15;
+    case 3:
+        return 20;
+    case 4:
+        return 25;
+
+    default:
+        return -1;
     }
 }
