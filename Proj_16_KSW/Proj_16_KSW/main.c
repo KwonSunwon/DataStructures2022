1cf4b2935103bdf5ab926c349a228a360037bbf7
KwonSunwon <kso00011147@gmail.com>
2022-05-12 22:08:33 +0900

    .

--- Proj_16_KSW/Proj_16_KSW/main.c
+++ Proj_16_KSW/Proj_16_KSW/main.c
@@ -4,10 +4,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #if PROB == 1
 
-#define SIZE 100
+#define SIZE 5
 
 typedef int element;
 
@@ -22,8 +23,7 @@ void init(heap *);
 int is_empty(heap *);
 int is_full(heap *);
 void insert(heap *, element);
-element delete (heap *);
-element find(heap *);
+element delete_heap(heap *);
 
 void heap_sort(element *, element *, int);
 void selection_sort(element *, element *, int);
@@ -32,7 +32,7 @@ int main(void)
 {
     element *list, *sorted_list;
     int i, s_time, e_time;
-    srand(100);
+    srand(15);
     list = (element *)malloc(sizeof(element) * SIZE);
     sorted_list = (element *)malloc(sizeof(element) * SIZE);
     for (i = 0; i < SIZE; i++)
@@ -43,10 +43,18 @@ int main(void)
     e_time = clock();
     printf("Heap Sort 걸린 시간 : %d msec\n", e_time - s_time);
 
+    for (int i = 0; i < SIZE; ++i)
+        printf("%d ", sorted_list[i]);
+    printf("\n");
+
     s_time = clock();
     selection_sort(list, sorted_list, SIZE);
     e_time = clock();
     printf("Selection Sort 걸린 시간 : %d msec\n", e_time - s_time);
+
+    for (int i = 0; i < SIZE; ++i)
+        printf("%d ", sorted_list[i]);
+    printf("\n");
 
     return 0;
 }
@@ -90,19 +98,56 @@ void insert(heap *heap, element item)
     heap->data[idx] = item;
 }
 
-element delete (heap *heap)
+element delete_heap(heap *heap)
 {
-    
-}
+    int parent, child;
+    element item, tmp;
 
-element find(heap *heap);
+    item = heap->data[1];
+    tmp = heap->data[heap->count--];
+    parent = 1;
+    child = 2;
+    while (child <= heap->count)
+    {
+        if (child < heap->count && heap->data[child] < heap->data[child + 1])
+            ++child;
+        if (tmp >= heap->data[child])
+            break;
+        heap->data[parent] = heap->data[child];
+        parent = child;
+        child *= 2;
+    }
+    heap->data[parent] = tmp;
+    return item;
+}
 
 void heap_sort(element *list, element *sorted_list, int size)
 {
+    heap *h = create();
+    init(h);
+    for (int i = 0; i < size; ++i)
+        insert(h, list[i]);
+
+    for (int i = 0; i < size; ++i)
+        sorted_list[i] = delete_heap(h);
 }
 
 void selection_sort(element *list, element *sorted_list, int size)
 {
+    int temp;
+    for (int i = 0; i < size - 1; ++i)
+    {
+        for (int j = i + 1; j < size; ++j)
+        {
+            if (list[i] > list[j])
+            {
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+        sorted_list[i] = list[i];
+    }
 }
 
 #elif PROB == 2
